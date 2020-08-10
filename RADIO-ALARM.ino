7adf149857497d261a73c729380411f3e3819b0c
Josh Bown <joshbown.94@outlook.com>
2020-08-10 16:37:08 +0100

    Initial Commit

---
+++ RADIO-ALARM.ino
@@ -0,0 +1,97 @@
+#include <Keypad.h>
+
+const byte ROWS = 4;
+const byte COLS = 4;
+
+const int MAX_CODE_LENGTH = 4;
+const int button = 2;
+const int buzzer = 11;
+
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte rowPins[ROWS] = {10, 9, 8, 7};
+byte colPins[COLS] = {6, 5, 4, 3};
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+char disarmCode[MAX_CODE_LENGTH] = {'0', '0', '0', '0'}; //TODO: Temporary - Look into defining on the device
+bool activated = false;
+
+char currentCode[MAX_CODE_LENGTH] = {};
+int currentCodeIdx = 0;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(button, INPUT);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  
+  Serial.begin(9600);
+  Serial.println("Status: Armed");
+}
+
+void loop() {
+  if (digitalRead(button) == HIGH) {
+    Serial.println("Status: Triggered");
+    activated = true;
+  }
+
+  if (activated) {
+    handleCodeEntry();
+    enableAlarm();
+  } else {
+    disableAlarm(false);
+  }
+}
+
+void enableAlarm() {
+  tone(buzzer, 50);
+}
+void disableAlarm(bool output) {
+  if (output) {
+    Serial.println("Status: Disarmed");
+  }
+  
+  activated = false;
+  noTone(buzzer);
+}
+
+void handleCodeEntry() {
+  char enteredKey = customKeypad.getKey();
+  bool hasDisarmed = false;
+  
+  if (enteredKey){
+    Serial.println(enteredKey);
+    
+    currentCode[currentCodeIdx] = enteredKey;
+    currentCodeIdx++;
+  }
+
+  if (currentCodeIdx > MAX_CODE_LENGTH-1) {
+    if (checkDisarmed()) {
+      disableAlarm(true);
+    } else {
+      Serial.println("Status: Disarm Unauthorised - Incorrect Code");
+    }
+  }
+}
+
+bool checkDisarmed() {
+   bool hasDisarmed = true;
+    
+  for (int i=0; i < currentCodeIdx; i++) {
+    if (currentCode[i] != disarmCode[i]) {
+      hasDisarmed = false;
+      break;
+    }
+  }
+
+  currentCodeIdx = 0;
+
+  return hasDisarmed;
+}
